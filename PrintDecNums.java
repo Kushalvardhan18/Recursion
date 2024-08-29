cd079884370f4a366231d1e1199fe32015a9adb6
Kushal Vardhan <vardhank140102@gmail.com>
2024-08-29 11:48:55 +0530

    printing decreasing numbers using recursion

--- PrintDecNums.java
+++ PrintDecNums.java
@@ -1,5 +1,16 @@
+//Print Numbers from n to 1.
 public class PrintDecNums {
+    public static void printDec(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n+" ");
+        printDec(n-1);
+
+    }
     public static void main(String[] args){
-        
+        int n =10;
+        printDec(n);
     }
 }
