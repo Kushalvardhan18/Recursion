8afaebf65136c9afbb808d95449ca6d965851cb2
Kushal Vardhan <vardhank140102@gmail.com>
2024-09-02 21:12:38 +0530

    Update SortedOrNot.java

--- SortedOrNot.java
+++ SortedOrNot.java
@@ -1,5 +1,13 @@
 public class SortedOrNot {
+    public static int sortFunc(int nums[]){
+        int n = nums.length;
+        if(n==1){
+            return n;
+        }
+        nums[0]<nums[1]+sortFunc(n-1);
+    }
     public static void main(String[] args){
-        
+        int nums[] = {4,7,9,8,1,2};
+        sortFunc(nums);
     }
 }
