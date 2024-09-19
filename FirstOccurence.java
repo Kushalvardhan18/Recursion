cc9766d500048e6d0f6dde1704280934b2af83ae
Kushal Vardhan <vardhank140102@gmail.com>
2024-09-19 11:49:41 +0530

    first ccurence of integer

---
+++ FirstOccurence.java
@@ -0,0 +1,21 @@
+public class FirstOccurence {
+    public static int firstOccurIndex(int nums[],int key,int i){
+        int n = nums.length;
+        if(i==n-1 ){
+            return -1;
+        }
+        if(nums[i] == key){
+            return i;
+        }
+
+          return  firstOccurIndex(nums,key,i+1);
+
+
+    }
+    public static void main(String[] args){
+        int nums[] ={8,3,6,9,10,2,3};
+        int key =5;
+        int i=0;
+        System.out.println(firstOccurIndex(nums,key,i));
+    }
+}
