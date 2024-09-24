c93e657f4430aa7eab21f0ae43951da8717c53fe
Kushal Vardhan <vardhank140102@gmail.com>
2024-09-24 13:33:23 +0530

    tiling problem

---
+++ TilingProblem.java
@@ -0,0 +1,19 @@
+public class TilingProblem {
+    public static int solution(int n){
+        if(n==0 || n==1){
+            return 1;
+        }
+        //kaam
+        //vertical choice
+        int verticalTiles = solution(n-1);
+
+        // horizontal choice
+        int horizontalChoice = solution(n-2);
+
+        int totalWays = verticalTiles +horizontalChoice;
+        return totalWays;
+    }
+    public static void main(String[] args){
+        System.out.println(solution(10));
+    }
+}
