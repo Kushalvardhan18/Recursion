88ecb57adf01ff9d9c07100edd5c11ad1b6a46d6
Kushal Vardhan <vardhank140102@gmail.com>
2024-09-25 20:26:08 +0530

    Duplicates in String

--- DuplicatesInString.java
+++ DuplicatesInString.java
@@ -1,9 +1,21 @@
 public class DuplicatesInString {
-    public static String removeDuplicates(String s){
-
+    public static void removeDuplicates(String str, int idx, StringBuilder newStr, boolean map[]) {
+        if (idx == str.length()) {
+            System.out.println(newStr);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if(map[currChar-'a'] == true){
+            removeDuplicates(str,idx+1,newStr,map);
+        }
+        else {
+            map[currChar-'a'] =true;
+            removeDuplicates(str,idx+1,newStr.append(currChar),map);
+        }
     }
-    public static void main(String[] args){
-        String s = new String("appnnacollege");
-        System.out.println(removeDuplicates(s));
+
+    public static void main(String[] args) {
+        String str = new String("appnnacollege");
+        removeDuplicates(str, 0, new StringBuilder(""), new boolean[26]);
     }
 }
